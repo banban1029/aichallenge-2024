4d397414c8824f9ddfdd75c737268cdf6cc97590
banban1029 <banshiro1029@gmail.com>
2024-08-18 00:28:25 +0900

    Add environment_status package for survey simulation environment

---
+++ aichallenge/workspace/src/aichallenge_submit/environment_status/environment_status/object_detector.py
@@ -0,0 +1,187 @@
+#!/usr/bin/env python3
+import rclpy
+import rclpy.node
+import rclpy.qos
+import rclpy.executors
+import csv
+import os
+from std_msgs.msg import Float64MultiArray
+from datetime import datetime
+
+class ObjectDetectorNode(rclpy.node.Node):
+    def __init__(self):
+        super().__init__("object_detector")
+        self.sub = self.create_subscription(Float64MultiArray, "/aichallenge/objects", self.callback, 1)
+        
+        # Prepare CSV file
+        output_dir = './workspace/src/aichallenge_submit/environment_status/environment_status/out/'
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+        csv_file_path = os.path.join(output_dir, f'objects_{timestamp}.csv')
+
+        self.csv_file = open(csv_file_path, mode='a', newline='')
+        self.csv_writer = csv.writer(self.csv_file)
+        self.csv_writer.writerow(['x', 'y', 'z', 'size'])  # Header of the CSV file
+
+    def callback(self, msg):
+        # Log the data length
+        self.get_logger().info(f"Received data length: {len(msg.data)}")
+
+        # Log the full data for debugging
+        self.get_logger().info(f"Received data: {msg.data}")
+
+        # Log the data
+        if len(msg.data) % 4 != 0:
+            self.get_logger().warning("Received data length is not a multiple of 4")
+
+        for i in range(0, len(msg.data), 4):
+            try:
+                x = msg.data[i + 0]
+                y = msg.data[i + 1]
+                z = msg.data[i + 2]
+                size = msg.data[i + 3]
+                # ログ出力
+                self.get_logger().info(f"Received object at ({x}, {y}, {z}) with size {size}")
+
+                # CSVファイルに書き込み
+                self.csv_writer.writerow([x, y, z, size])
+            except IndexError:
+                self.get_logger().error(f"Index error processing data at index {i}")
+
+    def close(self):
+        # Close CSV file when node is destroyed
+        if hasattr(self, 'csv_file') and self.csv_file is not None:
+            self.csv_file.close()
+
+class PitStopDetectorNode(rclpy.node.Node):
+
+    def __init__(self):
+        super().__init__("pitstop_detector")
+        qos = rclpy.qos.QoSProfile(depth=1, reliability=rclpy.qos.QoSReliabilityPolicy.RELIABLE, durability=rclpy.qos.QoSDurabilityPolicy.TRANSIENT_LOCAL)
+        self.sub = self.create_subscription(Float64MultiArray, "/aichallenge/pitstop/area", self.callback, qos)
+        
+        # Prepare CSV file
+        output_dir = './workspace/src/aichallenge_submit/environment_status/environment_status/out/'
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+        csv_file_path = os.path.join(output_dir, f'pitstop_{timestamp}.csv')
+        self.csv_file = open(csv_file_path, mode='a', newline='')
+        self.csv_writer = csv.writer(self.csv_file)
+        self.csv_writer.writerow(['Position X', 'Position Y', 'Position Z', 'Orientation X', 'Orientation Y', 'Orientation Z', 'Orientation W', 'Scale X', 'Scale Y', 'Scale Z'])
+
+    def callback(self, msg):
+        # Log the data length
+        self.get_logger().info(f"Received pitstop data length: {len(msg.data)}")
+
+        # Log the full data for debugging
+        self.get_logger().info(f"Received pitstop data: {msg.data}")
+
+        # Log the data
+        if len(msg.data) % 9 != 0:
+            self.get_logger().warning("Received pitstop data length is not a multiple of 9")
+
+        for i in range(0, len(msg.data), 9):
+            try:
+                x = msg.data[i + 0]
+                y = msg.data[i + 1]
+                z = msg.data[i + 2]
+                ox = msg.data[i + 3]
+                oy = msg.data[i + 4]
+                oz = msg.data[i + 5]
+                ow = msg.data[i + 6]
+                sx = msg.data[i + 7]
+                sy = msg.data[i + 8]
+                # ログ出力
+                self.get_logger().info(f"Received pitstop area at ({x}, {y}, {z}) with orientation ({ox}, {oy}, {oz}, {ow}) and scale ({sx}, {sy}, 0.1)")
+
+                # CSVファイルに書き込み
+                self.csv_writer.writerow([x, y, z, ox, oy, oz, ow, sx, sy, 0.1])
+            except IndexError:
+                self.get_logger().error(f"Index error processing pitstop data at index {i}")
+
+    def close(self):
+        # Close CSV file when node is destroyed
+        if hasattr(self, 'csv_file') and self.csv_file is not None:
+            self.csv_file.close()
+
+class PitstopStatusNode(rclpy.node.Node):
+
+    def __init__(self):
+        super().__init__("pitstop_status_node")
+        self.sub = self.create_subscription(Float64MultiArray, "/aichallenge/pitstop/status", self.callback, 1)
+        
+        # Prepare CSV file
+        output_dir = './workspace/src/aichallenge_submit/environment_status/environment_status/out/'
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+
+        timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
+        csv_file_path = os.path.join(output_dir, f'pitstop_status_{timestamp}.csv')
+        self.csv_file = open(csv_file_path, mode='a', newline='')
+        self.csv_writer = csv.writer(self.csv_file)
+        self.csv_writer.writerow(['Elapsed Time (s)', 'Pit Status'])  # Header of the CSV file
+
+        # Record the start time
+        self.start_time = datetime.now()
+
+    def callback(self, msg):
+        # Log the data length
+        self.get_logger().info(f"Received pitstop status data length: {len(msg.data)}")
+
+        # Log the full data for debugging
+        self.get_logger().info(f"Received pitstop status data: {msg.data}")
+
+        # Log the data
+        if len(msg.data) != 1:
+            self.get_logger().warning("Received pitstop status data length is not equal to 1")
+
+        try:
+            # Calculate elapsed time
+            current_time = datetime.now()
+            elapsed_time = 1
+            pit_status = msg.data[0]  # Assuming the status is in the first index
+            self.get_logger().info(f"Received pitstop status: {pit_status}")
+
+            # Write to CSV file
+            self.csv_writer.writerow([elapsed_time, pit_status])
+        except IndexError:
+            self.get_logger().error("Index error processing pitstop status data")
+
+    def close(self):
+        # Close CSV file when node is destroyed
+        if hasattr(self, 'csv_file') and self.csv_file is not None:
+            self.csv_file.close()
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    executor = rclpy.executors.MultiThreadedExecutor()
+    
+    # ノードの作成
+    object_detector_node = ObjectDetectorNode()
+    pitstop_detector_node = PitStopDetectorNode()
+    # pitstop_status_node = PitstopStatusNode()
+
+    
+    executor.add_node(object_detector_node)
+    executor.add_node(pitstop_detector_node)
+    # executor.add_node(pitstop_status_node)
+    
+    try:
+        executor.spin()
+    except KeyboardInterrupt:
+        # Handle ctrl+c
+        pass
+    finally:
+        # Clean up resources
+        object_detector_node.close()
+        pitstop_detector_node.close()
+        # pitstop_status_node.close()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
